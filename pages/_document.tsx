7726bb1914d7e3dda3fe1f8d698da0700b5b2cfb
Dillon Grannis <dillongrannis@gmail.com>
2026-02-03 13:13:45 -0600

    Add Google Tag Manager (GTM-W6R6H3F7)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,18 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W6R6H3F7');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
         {/* Google tag (gtag.js) */}
         <script
           async
@@ -21,6 +33,17 @@ export default function Document() {
         />
       </Head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W6R6H3F7"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        
         <Main />
         <NextScript />
       </body>
